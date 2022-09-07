31b0542a8fcae449c2c9fe2e870ad2356565a8af
RISHIGOLI <rushikeshgoli38@gmail.com>
2022-09-07 17:52:49 +0530

    commit before changing structure

--- src/main/java/com/mycode/blog/payloads/UserDto.java
+++ src/main/java/com/mycode/blog/payloads/UserDto.java
@@ -5,9 +5,8 @@ import java.util.Set;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
 
 import com.mycode.blog.entities.Role;
 
--- src/main/java/com/mycode/blog/security/CustomUserDetailService.java
+++ src/main/java/com/mycode/blog/security/CustomUserDetailService.java
@@ -20,7 +20,7 @@ public class CustomUserDetailService implements UserDetailsService{
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		//loading user from database by username
-		 User user = this.userRepo.findByEmail(username).orElseThrow(()-> new ResourceNotFoundException("user", "email : "+ username, 0));
+		User user = this.userRepo.findByEmail(username).orElseThrow(()-> new ResourceNotFoundException("User", "email :" +username, 0));
 		 return user;
 		
 	}
